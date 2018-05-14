d1dfca9a3de7d5b5519900f53dc924b6d7c66664
Victoria Nazarova <shootyourstereo@gmail.com>
2018-05-13 21:24:50 -0400

    hw4_rockpaperscissors

--- hw1_money/js/script.js
+++ hw1_money/js/script.js
@@ -1,10 +1,17 @@
-	let money = prompt('Сколько у вас с cобой денег?');
-	let apples = prompt('Сколько вы купили яблок?');
-	let bread = prompt('Сколько вы купили батонов хлеба?');
-	let apple = prompt('Сколько стоит одно яблоко?');
-	let loaf = prompt('Сколько стоит один батон?');
-	let allapples = parseInt(apple) * parseFloat(apples);
-	let allbread = parseFloat(bread) * parseFloat(loaf);
+	const money = prompt('Сколько у вас с cобой денег?');
+	const apples = prompt('Сколько вы купили яблок?');
+	const bread = prompt('Сколько вы купили батонов хлеба?');
+	const apple = prompt('Сколько стоит одно яблоко?');
+	const loaf = prompt('Сколько стоит один батон?');
+	const allapples = parseInt(apple) * parseFloat(apples);
+	const allbread = parseFloat(bread) * parseFloat(loaf);
 	let result = parseFloat(money) >= allapples+allbread;
 	let res = result.toString();
 	document.getElementById("final").innerHTML = res;
+
+	let back = document.querySelector('body');
+	if(result===true) {
+		back.style.backgroundColor = 'green'
+	}else {
+		back.style.backgroundColor = 'red'; 
+	}
