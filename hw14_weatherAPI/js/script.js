4bb6b85510ee545b4de902d5e5984e56297ec6dc
Victoria Nazarova <shootyourstereo@gmail.com>
2018-06-04 15:43:36 +0300

    hw14 correct

---
+++ hw14_weatherAPI/js/script.js
@@ -0,0 +1,22 @@
+
+const APIKey = 'dfaedf9a05aa4f228dd175432180306';
+const form = document.forms[0];
+const insert = document.querySelector('.temp');
+
+form.onsubmit = function(e) {
+	e.preventDefault();
+	let result = form.elements.city.value;
+	const url = 'http://api.apixu.com/v1/current.json?key=' + APIKey + '&q=' + result;
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', url, false);
+	xhr.send();
+
+if (xhr.status != 200){
+	console.log(xhr.status + ' ' + xhr.statusText);
+}
+else {
+	let DATA = JSON.parse(xhr.responseText);
+	console.log(DATA);
+	insert.innerHTML = DATA.current.temp_c;
+}
+}
